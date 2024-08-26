92fe6f5aa18461ee7cd3b986bbcce30da7865d30
tiagoa-ss <tiago201andre@gmail.com>
2024-08-26 15:25:46 -0300

    Added types

---
+++ src/types/pokemonTypes.enum.ts
@@ -0,0 +1,100 @@
+export enum PokemonTypes {
+	normal = 'normal',
+	fighting = 'fighting',
+	flying = 'flying',
+	poison = 'poison',
+	ground = 'ground',
+	rock = 'rock',
+	bug = 'bug',
+	ghost = 'ghost',
+	steel = 'steel',
+	fire = 'fire',
+	water = 'water',
+	grass = 'grass',
+	electric = 'electric',
+	psychic = 'psychic',
+	ice = 'ice',
+	dragon = 'dragon',
+	dark = 'dark',
+	fairy = 'fairy',
+	stellar = 'stellar',
+}
+
+export const PokemonTypesMap = new Map<PokemonTypes, string>([
+	[
+		PokemonTypes.normal,
+		'https://archives.bulbagarden.net/media/upload/thumb/9/95/Normal_icon_SwSh.png/120px-Normal_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.fighting,
+		'https://archives.bulbagarden.net/media/upload/thumb/3/3b/Fighting_icon_SwSh.png/120px-Fighting_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.flying,
+		'https://archives.bulbagarden.net/media/upload/thumb/b/b5/Flying_icon_SwSh.png/120px-Flying_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.poison,
+		'https://archives.bulbagarden.net/media/upload/thumb/8/8d/Poison_icon_SwSh.png/120px-Poison_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.ground,
+		'https://archives.bulbagarden.net/media/upload/thumb/2/27/Ground_icon_SwSh.png/120px-Ground_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.rock,
+		'https://archives.bulbagarden.net/media/upload/thumb/1/11/Rock_icon_SwSh.png/120px-Rock_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.bug,
+		'https://archives.bulbagarden.net/media/upload/thumb/9/9c/Bug_icon_SwSh.png/120px-Bug_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.ghost,
+		'https://archives.bulbagarden.net/media/upload/thumb/0/01/Ghost_icon_SwSh.png/120px-Ghost_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.steel,
+		'https://archives.bulbagarden.net/media/upload/thumb/0/09/Steel_icon_SwSh.png/120px-Steel_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.fire,
+		'https://archives.bulbagarden.net/media/upload/thumb/a/ab/Fire_icon_SwSh.png/120px-Fire_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.water,
+		'https://archives.bulbagarden.net/media/upload/thumb/8/80/Water_icon_SwSh.png/120px-Water_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.grass,
+		'https://archives.bulbagarden.net/media/upload/thumb/a/a8/Grass_icon_SwSh.png/120px-Grass_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.electric,
+		'https://archives.bulbagarden.net/media/upload/thumb/7/7b/Electric_icon_SwSh.png/120px-Electric_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.psychic,
+		'https://archives.bulbagarden.net/media/upload/thumb/7/73/Psychic_icon_SwSh.png/120px-Psychic_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.ice,
+		'https://archives.bulbagarden.net/media/upload/thumb/1/15/Ice_icon_SwSh.png/120px-Ice_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.dragon,
+		'https://archives.bulbagarden.net/media/upload/thumb/7/70/Dragon_icon_SwSh.png/120px-Dragon_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.dark,
+		'https://archives.bulbagarden.net/media/upload/thumb/d/d5/Dark_icon_SwSh.png/120px-Dark_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.fairy,
+		'https://archives.bulbagarden.net/media/upload/thumb/c/c6/Fairy_icon_SwSh.png/120px-Fairy_icon_SwSh.png',
+	],
+	[
+		PokemonTypes.stellar,
+		'https://archives.bulbagarden.net/media/upload/4/4e/Stellar_icon_SV.png',
+	],
+]);
