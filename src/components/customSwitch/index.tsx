44d595bac18d77f7a371fd4cb791e49b10a31b5a
tiagoa-ss <tiago201andre@gmail.com>
2024-10-28 11:28:38 -0300

    Added S3 images

--- src/components/customSwitch/index.tsx
+++ src/components/customSwitch/index.tsx
@@ -1,7 +1,5 @@
 import { Switch } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import starsIcon from '../../assets/stars.svg';
-import starsBlackIcon from '/stars-black.svg';
 
 interface ICustomSwitch {
 	toggleShiny: () => void;
@@ -20,7 +18,7 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
 			color: '#fff',
 			transform: 'translateX(22px)',
 			'& .MuiSwitch-thumb:before': {
-				backgroundImage: `url(${starsIcon})`,
+				backgroundImage: `url(${'https://pokemons-assets-313462056628.s3-accesspoint.sa-east-1.amazonaws.com/stars.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAUR66LM22JUPHY2W5%2F20241028%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241028T142706Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEM%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXNhLWVhc3QtMSJHMEUCIBqZ8gLv%2FmiaxsCIH2R0JVN%2Fkl1hSbHUEE%2BGnoJp7VTLAiEAlX%2F2ufeFEjmId4qsi7mF4vdoPzZCaNmk2Gk%2B1HF1Jmsq6AIISBABGgwzMTM0NjIwNTY2MjgiDDb7TgEGnxJfbLRPzCrFAue42qLmJy8XC9wIBS6VVQmjl8MtBVi7QzhSWUJrGoPqm1j9uSEhJqfEELS3dkqUoz093szNSJtSSUT8TYySXqzSmYBB1DfdOOhDwLLoS3aSkVS51TuRZqTfnuvUjB442kS1UJg6Q%2BIZQxezAUH8Mh70zkUf8JRR8NCXvey3HdfaxKMN8bdgdkowVNXjq2nh0FNBlfD3kFwOqJT3zvz8Jf13iskS7BMY6MhT0gRCucPQInf4s%2BkypQlI5Y0wBXJO3t6PMDoQ60I01kawyCpO8arRDHCf7wxH9zqbT49L8Jg9pU4k1gGHTJfbMboauQHSTGcsN5h7LvmCyKhtEeNBBy6w6AVp5FgaN%2FIKSSdu%2BB0pdKT24A6NYWFE0T%2BonlzI19nqpLKi%2BCoI1Tn5foqNt2D5vOqi4WQmMUOOgD1PoMhASY8axYcw87L%2BuAY6swITEOL%2FSHNDc6wUQdJEGm3VwPirri%2F4vMFLMRjHvPA0u56YqFhgUcweOMa2fMNy%2F7zzoPQQ912Bb7t27SHcAyv%2BrDFYq3w0TDn9vfBRA4h33Jgv1t5%2Fbriot0Bd7Nkm2xR5TUnKkLvbrE5kgi4fQwUQvStbATullJDMKVVxb9IFgvFqlKQ0yXi5%2FGfmQkw1vpAgWDjzrBsk7Q0xxWJFLU%2FuBghtmll8%2FnOLYrFRYesaOhb7VvI2g8r0HU%2BLl4jXO41jh8peweE9wcmabVr4ZF6gtRWsY6Ebzbf1ZJeNLPEL5%2B77gcDJOcVLIMUlLyX7lTVKW2VVRUHC7H%2F6GxgLUEJGUlcvShWpyiuXcwA5PnfKmCJ3jXkoOtZ4h0BCVIyotgzKUz%2B0VycKS9IbZXKfUnOqk6YG&X-Amz-Signature=fe050475669ddc0b7e8ca9c94d3091d5da123b21b301312189eb3bf02e12164c&X-Amz-SignedHeaders=host&response-content-disposition=inline'})`,
 				backgroundSize: '20px',
 			},
 			'& + .MuiSwitch-track': {
@@ -45,7 +43,7 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
 			top: 0,
 			backgroundRepeat: 'no-repeat',
 			backgroundPosition: 'center',
-			backgroundImage: `url(${starsBlackIcon})`,
+			backgroundImage: `url(${'https://pokemons-assets-313462056628.s3-accesspoint.sa-east-1.amazonaws.com/stars-black.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAUR66LM22JUPHY2W5%2F20241028%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241028T142630Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEM%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXNhLWVhc3QtMSJHMEUCIBqZ8gLv%2FmiaxsCIH2R0JVN%2Fkl1hSbHUEE%2BGnoJp7VTLAiEAlX%2F2ufeFEjmId4qsi7mF4vdoPzZCaNmk2Gk%2B1HF1Jmsq6AIISBABGgwzMTM0NjIwNTY2MjgiDDb7TgEGnxJfbLRPzCrFAue42qLmJy8XC9wIBS6VVQmjl8MtBVi7QzhSWUJrGoPqm1j9uSEhJqfEELS3dkqUoz093szNSJtSSUT8TYySXqzSmYBB1DfdOOhDwLLoS3aSkVS51TuRZqTfnuvUjB442kS1UJg6Q%2BIZQxezAUH8Mh70zkUf8JRR8NCXvey3HdfaxKMN8bdgdkowVNXjq2nh0FNBlfD3kFwOqJT3zvz8Jf13iskS7BMY6MhT0gRCucPQInf4s%2BkypQlI5Y0wBXJO3t6PMDoQ60I01kawyCpO8arRDHCf7wxH9zqbT49L8Jg9pU4k1gGHTJfbMboauQHSTGcsN5h7LvmCyKhtEeNBBy6w6AVp5FgaN%2FIKSSdu%2BB0pdKT24A6NYWFE0T%2BonlzI19nqpLKi%2BCoI1Tn5foqNt2D5vOqi4WQmMUOOgD1PoMhASY8axYcw87L%2BuAY6swITEOL%2FSHNDc6wUQdJEGm3VwPirri%2F4vMFLMRjHvPA0u56YqFhgUcweOMa2fMNy%2F7zzoPQQ912Bb7t27SHcAyv%2BrDFYq3w0TDn9vfBRA4h33Jgv1t5%2Fbriot0Bd7Nkm2xR5TUnKkLvbrE5kgi4fQwUQvStbATullJDMKVVxb9IFgvFqlKQ0yXi5%2FGfmQkw1vpAgWDjzrBsk7Q0xxWJFLU%2FuBghtmll8%2FnOLYrFRYesaOhb7VvI2g8r0HU%2BLl4jXO41jh8peweE9wcmabVr4ZF6gtRWsY6Ebzbf1ZJeNLPEL5%2B77gcDJOcVLIMUlLyX7lTVKW2VVRUHC7H%2F6GxgLUEJGUlcvShWpyiuXcwA5PnfKmCJ3jXkoOtZ4h0BCVIyotgzKUz%2B0VycKS9IbZXKfUnOqk6YG&X-Amz-Signature=9ac3e54c28d439639b5154298bbdfdd71fd9ff3e1c92f0fafdca50a5599e4a0a&X-Amz-SignedHeaders=host&response-content-disposition=inline'})`,
 			backgroundSize: '20px',
 		},
 		...theme.applyStyles('dark', {
